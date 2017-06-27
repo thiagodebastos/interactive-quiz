e068827fa1956c953d317bf9cf8d5be03404c855
Thiago de Bastos <thiago@thiagodebastos.com>
2017-06-27 10:20:51 +1000

    replace devtools with browser extension

--- src/containers/Root.dev.js
+++ src/containers/Root.dev.js
@@ -5,23 +5,21 @@ import { Route } from 'react-router-dom';
 import { ConnectedRouter } from 'react-router-redux';
 
 import App from '../components/App';
-import DevTools from './DevTools';
 
 type Props = {
-    store: Object,
-    history: Object
+  store: Object,
+  history: Object
 };
 
 export default function Root(props: Props) {
-    const { store, history } = props;
-    return (
-        <Provider store={store}>
-            <div>
-                <ConnectedRouter history={history}>
-                    <Route path="/" component={App} />
-                </ConnectedRouter>
-                <DevTools />
-            </div>
-        </Provider>
-    );
+  const { store, history } = props;
+  return (
+    <Provider store={store}>
+      <div>
+        <ConnectedRouter history={history}>
+          <Route path="/" component={App} />
+        </ConnectedRouter>
+      </div>
+    </Provider>
+  );
 }
--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -2,16 +2,18 @@ import createHistory from 'history/createBrowserHistory';
 import { applyMiddleware, createStore, compose } from 'redux';
 import { routerMiddleware } from 'react-router-redux';
 import rootReducer from '../reducers';
-import DevTools from '../containers/DevTools';
 
 export const history = createHistory();
 const middleware = routerMiddleware(history);
 
-export function configureStore(initialState) {
-    return createStore(
-        rootReducer,
-        initialState,
-        compose(applyMiddleware(middleware), DevTools.instrument())
-    );
-}
+/* eslint-disable no-underscore-dangle */
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+/* eslint-enable */
 
+export function configureStore(initialState) {
+  return createStore(
+    rootReducer,
+    initialState,
+    composeEnhancers(applyMiddleware(middleware))
+  );
+}
